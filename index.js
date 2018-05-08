8f4b254e28fece4cce14386a7127eb5302bde82a
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-05-08 11:12:59 -0300

    corrected comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // This index is the example app index,
-// if you look for react-naitve-camera-kit index,
+// if you look for react-native-camera-kit index,
 // please jump src/index 😅
 require('./example-js-code/app');
